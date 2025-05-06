3daf97d177e9c1594a8c5d54743269c463e3e7ae
zablon <zablonoigo51@gmail.com>
2025-05-06 22:16:54 +0300

    Add config

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,16 @@
+import os 
+from dotenv import load_dotenv
 
-
+load_dotenv()
 
 class Settings:
     PROJECT_NAME: str = ""
     PROJECT_VERSION: str = "1.0.0"
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASS: str = os.getenv("DB_PASS")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_URL: str = os.getenv("DB_URL")
 
 settings=Settings()
