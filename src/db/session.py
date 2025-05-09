608ae76b88e9df76fe82984877ca50e50d9400c1
zablon <zablonoigo51@gmail.com>
2025-05-09 10:38:35 +0300

    add connection to db

---
+++ src/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
+
+from core.config import settings 
+
+DATABASE_URL=settings.DB_URL 
+engine=create_engine(DATABASE_URL)
+sessionLoca=sessionmaker(autocommit=False, autoflush=False,bind=engine)
