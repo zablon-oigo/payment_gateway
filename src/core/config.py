b7b7b7ed2b89d96b9c27fc03075afd2caa1215d1
zablon <zablonoigo51@gmail.com>
2025-05-10 14:19:25 +0300

    add default var

--- src/core/config.py
+++ src/core/config.py
@@ -1,16 +1,28 @@
-import os 
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-load_dotenv()
-
-class Settings:
-    PROJECT_NAME: str = ""
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Payment API"
     PROJECT_VERSION: str = "1.0.0"
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASS: str = os.getenv("DB_PASS")
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT")
-    DB_NAME: str = os.getenv("DB_NAME")
-    DB_URL: str = os.getenv("DB_URL")
 
-settings=Settings()
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+    PORT: int = 8000 
+
+    @property
+    def DB_URL(self):
+        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    SAFARICOM_BASE_URL: str
+    CONSUMER_KEY: str
+    SECRET: str
+    PAYBILL: str
+    PASSKEY: str
+    CALLBACK_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
