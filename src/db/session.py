b2e304c1520551120c020eedca09258a609eb6ce
zablon <zablonoigo51@gmail.com>
2026-08-06 18:15:00 +0300

    delete temp files

--- src/db/session.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine 
-from sqlalchemy.orm import sessionmaker 
-
-from core.config import settings 
-
-DATABASE_URL = settings.DB_URL
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
